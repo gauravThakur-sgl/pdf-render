ccc953d8cde3b98354f3b083034697a91db3626b
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2024-10-01 16:32:02 +0530

    Added lazy loading to make little efficient

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import { useState } from "react"
+// import PdfRender from "./components/PdfRender";
+import{ lazy, Suspense } from 'react';
+const MyLazyComponent = lazy(() => import('./components/PdfRender'));
 
-function PdfRender() {
-  return(
-    <div className="max-md:h-screen max-md:w-auto">
-      <iframe src= "https://file-examples.com/storage/fecf122cd366fad119b983a/2017/10/file-sample_150kB.pdf"
-        height={900} width={800}
-      >
-      </iframe>
-    </div>
-  )
-}
+
 function App() {
   const [render,setRender] = useState(false);
   // const pdfUrl = "https://file-examples.com/storage/fecf122cd366fad119b983a/2017/10/file-sample_150kB.pdf";
   return (
     <>
-      <div className="flex flex-col justify-center items-center p-4 m-10">
+      {/* <div className="flex flex-col justify-center items-center p-4 m-10">
         <button onClick={() => setRender(true)} className="border rounded-lg p-2 hover:bg-black-500">Click me to Read the Agreement</button>
         <div>
           {render && <PdfRender />}
         </div>
+      </div> */}
+      <div className="flex flex-col justify-center items-center p-4 m-10">
+        <button onClick={() => setRender(true)} className="border rounded-lg p-2">Click me</button>
+        <div>
+          {render && <Suspense fallback={<div>Loading...</div>}><MyLazyComponent /></Suspense>}
+        </div>
       </div>
+      
     </>
   )
 }
---
+++ src/components/PdfRender.jsx
@@ -0,0 +1,12 @@
+export default function PdfRender() {
+  return (
+    <div className="max-md:h-screen max-md:w-auto">
+      <iframe
+        src="https://file-examples.com/storage/fecf122cd366fad119b983a/2017/10/file-sample_150kB.pdf"
+        
+        height={900}
+        width={800}
+      ></iframe>
+    </div>
+  );
+}
